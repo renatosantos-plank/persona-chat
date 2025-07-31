317a37dff0f8e5fc48f392348750381aa40f83ea
Renato Santos <renato.santos@joinplank.com>
2025-07-31 11:01:11 -0300

    refactor : move `layout.tsx` to root folder and changes Metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist } from "next/font/google";
 import { ThemeProvider } from "next-themes";
-import "../globals.css";
+import "./globals.css";
 
 const defaultUrl = process.env.VERCEL_URL
   ? `https://${process.env.VERCEL_URL}`
@@ -9,8 +9,8 @@ const defaultUrl = process.env.VERCEL_URL
 
 export const metadata: Metadata = {
   metadataBase: new URL(defaultUrl),
-  title: "Next.js and Supabase Starter Kit",
-  description: "The fastest way to build apps with Next.js and Supabase",
+  title: "Bat Agent",
+  description: "i have no idea",
 };
 
 const geistSans = Geist({
